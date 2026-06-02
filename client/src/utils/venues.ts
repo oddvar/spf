0c74cc8c1678602e3f31ae094045ea4b2e31b56d
Oddvar Lovaas <oddvar@gmail.com>
2026-06-02 09:40:06 +0100

    add ALL the stadium links

--- client/src/utils/venues.ts
+++ client/src/utils/venues.ts
@@ -10,25 +10,25 @@ export const VENUE_LINKS: Record<string, string> = {
   "Levi's Stadium, Santa Clara":
     'https://www.google.com/maps/place/San+Francisco+Bay+Area+Stadium/@35.5818096,-128.3871462,5z/data=!4m6!3m5!1s0x808fc9c827c5f0df:0x11455a372e1f7d18!8m2!3d37.4033165!4d-121.9693774!16s%2Fm%2F0269w0y?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'MetLife Stadium, East Rutherford':
-    'https://www.google.com/maps/place/MetLife+Stadium/@40.8128,-74.0764',
+    'https://www.google.com/maps/place/New+York+New+Jersey+Stadium/@40.9738217,-84.6295763,5z/data=!4m6!3m5!1s0x89c2f861ad5902bf:0x8184280d31051734!8m2!3d40.8135064!4d-74.074457!16zL20vMGRua3Bo?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Gillette Stadium, Foxborough':
-    'https://www.google.com/maps/place/Gillette+Stadium/@42.0878,-71.2587',
+    'https://www.google.com/maps/place/Boston+Stadium/@42.2520949,-81.818707,5z/data=!4m6!3m5!1s0x89e4632476f73d67:0x68f66bebfe4bcade!8m2!3d42.0909274!4d-71.264363!16zL20vMDI5OW03?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'BC Place, Vancouver':
-    'https://www.google.com/maps/place/BC+Place/@49.2767,-123.1124',
+    'https://www.google.com/maps/place/BC+Place+Vancouver/@49.4367192,-133.6618794,5z/data=!3m1!5s0x5486717ce3307959:0xbb811745f65fed95!4m6!3m5!1s0x5486717cfcbee0d5:0xf9404dfd918218bb!8m2!3d49.27675!4d-123.111999!16zL20vMDNiNGcz?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'NRG Stadium, Houston':
-    'https://www.google.com/maps/place/NRG+Stadium/@29.6842,-95.4065',
+    'https://www.google.com/maps/place/Houston+Stadium/@29.8244295,-105.9720813,5z/data=!4m6!3m5!1s0x8640c0194b406bad:0x1b0f6ec2399a9d57!8m2!3d29.6847219!4d-95.4107074!16zL20vMDF0eGoy?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'AT&T Stadium, Arlington':
-    'https://www.google.com/maps/place/AT%26T+Stadium/@32.7477,-97.0917',
+    'https://www.google.com/maps/place/Dallas+Stadium/@32.8956927,-107.6525761,5z/data=!4m6!3m5!1s0x864e7d86c8884727:0x6d1e60e88c6a9df8!8m2!3d32.7480281!4d-97.0928124!16zL20vMGc5djFx?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Lincoln Financial Field, Philadelphia':
-    'https://www.google.com/maps/place/Lincoln+Financial+Field/@39.9009,-75.1681',
+    'https://www.google.com/maps/place/Philadelphia+Stadium/@40.0609264,-85.7232666,5z/data=!4m6!3m5!1s0x89c6c7a9e03add31:0xf4050036d4293709!8m2!3d39.9014189!4d-75.1675992!16zL20vMDFsYzhw?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Estadio BBVA, Guadalupe':
-    'https://www.google.com/maps/place/Estadio+BBVA/@25.6692,-100.2444',
+    'https://www.google.com/maps/place/Estadio+Monterrey/@25.7960211,-110.8077624,5z/data=!4m6!3m5!1s0x8662c019f7a4c8d7:0xbf478dc0c2c5944d!8m2!3d25.6691508!4d-100.2444429!16s%2Fm%2F03c9trf?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Mercedes-Benz Stadium, Atlanta':
-    'https://www.google.com/maps/place/Mercedes-Benz+Stadium/@33.7555,-84.4020',
+    'https://www.google.com/maps/place/Atlanta+Stadium/@33.9052428,-94.9598039,5z/data=!4m6!3m5!1s0x88f5037c38c799e3:0x6541be00805df0ee!8m2!3d33.7553232!4d-84.4005905!16s%2Fm%2F0n4844m?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Lumen Field, Seattle':
-    'https://www.google.com/maps/place/Lumen+Field/@47.5951,-122.3322',
+    'https://www.google.com/maps/place/Seattle+Stadium/@47.7563113,-132.8825737,5z/data=!3m1!5s0x54906aa2524f7d43:0xd367e02efd54b8a4!4m6!3m5!1s0x54906aa3b9f1182b:0xa636cd513bba22dc!8m2!3d47.5951518!4d-122.3316394!16zL20vMDFkYl9q?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Hard Rock Stadium, Miami Gardens':
-    'https://www.google.com/maps/place/Hard+Rock+Stadium/@25.9580,-80.2393',
+    'https://www.google.com/maps/place/Miami+Stadium/@26.0857448,-90.8021409,5z/data=!4m6!3m5!1s0x88d9af0497b5f3a9:0x5815e194c60bf9db!8m2!3d25.957863!4d-80.2389542!16zL20vMDFfa3pt?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
   'Arrowhead Stadium, Kansas City':
-    'https://www.google.com/maps/place/Arrowhead+Stadium/@39.0488,-94.4846',
+    'https://www.google.com/maps/place/Kansas+City+Stadium/@39.2075783,-105.0401898,5z/data=!4m6!3m5!1s0x87c0e4a194db922b:0xbccd4d8cf72544e6!8m2!3d39.0489647!4d-94.4840141!16zL20vMDFxdHZu?entry=ttu&g_ep=EgoyMDI2MDUzMS4wIKXMDSoASAFQAw%3D%3D',
 };
